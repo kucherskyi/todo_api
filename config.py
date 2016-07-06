aebb96262c639d4fc875571900b508fd161d1e11
Artem Kucherskyi <a.kucherskyy@gmail.com>
2016-07-06 10:20:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ DEBUG = True
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
-DATABASE_CONNECT_OPTIONS = {}
 SQLALCHEMY_COMMIT_ON_TEARDOWN= True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
