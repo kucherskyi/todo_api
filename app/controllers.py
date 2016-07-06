4216683bbfc970e78f346f01699ac01066660563
artem <artem@ubuntu>
2016-07-06 10:18:48 +0300

    ..

--- app/controllers.py
+++ app/controllers.py
@@ -2,8 +2,6 @@
 
 import datetime
 from flask import Flask, jsonify, abort, make_response, request, g, url_for
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.httpauth import HTTPBasicAuth
 from passlib.apps import custom_app_context as pwd_context
 import os
 from app import db
